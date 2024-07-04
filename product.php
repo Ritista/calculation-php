2f4351257ce49edb777bd39b668d15b1984e76cf
Ritista Shrestha <ritista99@gmail.com>
2024-07-04 17:58:17 +0545

    first commit

---
+++ product.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+function product($num1, $num2, $num3)
+{
+    $product =$num1*$num2*$num3;
+    echo "the product is $product";
+}
+product(2,3,5);
+?>
+</body>
+</html>
---
+++ tax.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?php
+$price=100;$tax=0.10;
+Printf('the dish cost $%.4f', $price*(1+$tax));
+?>
+</body.
+</html>
